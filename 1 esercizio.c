8ff418dbb6e3cb7a93c2ac007d61e861d3778b47
Agent017-Gaming <fahmida596@gmail.com>
2024-09-17 16:45:23 +0200

    esercizio.c

---
+++ 1 esercizio.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+
+
+
+int main(void){
+	int N1;
+	int N2;
+	printf("Give me the 1st Number You are Thinking \n ");
+	scanf("%d", &N1);
+	printf("Give me the 2nd Number You are Thinking  after that \n ");
+	scanf("%d", &N2);
+	return 0;
+}
+
