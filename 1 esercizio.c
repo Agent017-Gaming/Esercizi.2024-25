b9a431cea89d91c045f70e148e77241c5f635743
Agent017-Gaming <fahmida596@gmail.com>
2024-09-17 20:49:17 +0200

    main1.c

--- 1 esercizio.c
+++ 1 esercizio.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+int Numeri(int N1, int N2){
+if (N2 % N1 == 0) {
+   return 1;
+  } else {
+    return 0;
+  }
+}
 
 int main(void){
 	int N1;
@@ -8,11 +15,11 @@ int main(void){
 	scanf("%d", &N1);
 	printf("Give me the 2nd Number You are Thinking  after that \n ");
 	scanf("%d", &N2);
-	
-	if (N1 % N2 == 0) {
-    printf("%d is a multiple of %d\n", N2, N1);
-  } else {
-    printf("%d is not a multiple of %d\n", N2, N1);
-  }
-	return 0;
+	int Res = Numeri(N1,N2);
+	if (Res) {
+	  printf("%d is a multiple of %d\n", N2, N1);
+	} else {
+	  printf("%d is not a multiple of %d\n", N2, N1);
+	}
+  return 0;
 }
